a7e99811653e2436bb1e9e9f990fdc45f98be9f0
racsoraul <racsoraul@gmail.com>
2019-01-25 12:01:17 -0600

    MultiAgent demo accepts vehicle name to control.

--- MultiAgent/main.cpp
+++ MultiAgent/main.cpp
@@ -31,52 +31,27 @@ int main()
     try {
         client.confirmConnection();
 
-        std::cout << "Press Enter to get FPV image" << std::endl; std::cin.get();
-        vector<ImageRequest> request = { ImageRequest("0", ImageType::Scene), ImageRequest("1", ImageType::DepthPlanner, true) };
-        const vector<ImageResponse>& response = client.simGetImages(request);
-        std::cout << "# of images received: " << response.size() << std::endl;
-
-        if (response.size() > 0) {
-            std::cout << "Enter path with ending separator to save images (leave empty for no save)" << std::endl; 
-            std::string path;
-            std::getline(std::cin, path);
-
-            for (const ImageResponse& image_info : response) {
-                std::cout << "Image uint8 size: " << image_info.image_data_uint8.size() << std::endl;
-                std::cout << "Image float size: " << image_info.image_data_float.size() << std::endl;
-
-                if (path != "") {
-                    std::string file_path = FileSystem::combine(path, std::to_string(image_info.time_stamp));
-                    if (image_info.pixels_as_float) {
-                        Utils::writePfmFile(image_info.image_data_float.data(), image_info.width, image_info.height,
-                            file_path + ".pfm");
-                    }
-                    else {
-                        std::ofstream file(file_path + ".png", std::ios::binary);
-                        file.write(reinterpret_cast<const char*>(image_info.image_data_uint8.data()), image_info.image_data_uint8.size());
-                        file.close();
-                    }
-                }
-            }
-        }
+        std::cout << "Enter name of the vehicle to control" << std::endl; 
+        std::string vehicle_name;
+        std::getline(std::cin, vehicle_name);
 
         std::cout << "Press Enter to arm the drone" << std::endl; std::cin.get();
-        client.enableApiControl(true);
-        client.armDisarm(true);
+        client.enableApiControl(true, vehicle_name);
+        client.armDisarm(true, vehicle_name);
 
         std::cout << "Press Enter to takeoff" << std::endl; std::cin.get();
         float takeoffTimeout = 5; 
-        client.takeoffAsync(takeoffTimeout)->waitOnLastTask();
+        client.takeoffAsync(takeoffTimeout, vehicle_name)->waitOnLastTask();
 
         // switch to explicit hover mode so that this is the fall back when 
         // move* commands are finished.
         std::this_thread::sleep_for(std::chrono::duration<double>(5));
-        client.hoverAsync()->waitOnLastTask();
+        client.hoverAsync(vehicle_name)->waitOnLastTask();
 
         std::cout << "Press Enter to fly in a 10m box pattern at 3 m/s velocity" << std::endl; std::cin.get();
         // moveByVelocityZ is an offboard operation, so we need to set offboard mode.
-        client.enableApiControl(true); 
-        auto position = client.getMultirotorState().getPosition();
+        client.enableApiControl(true, vehicle_name); 
+        auto position = client.getMultirotorState(vehicle_name).getPosition();
         float z = position.z(); // current position (NED coordinate system).  
         const float speed = 3.0f;
         const float size = 10.0f; 
@@ -84,25 +59,25 @@ int main()
         DrivetrainType driveTrain = DrivetrainType::ForwardOnly;
         YawMode yaw_mode(true, 0);
         std::cout << "moveByVelocityZ(" << speed << ", 0, " << z << "," << duration << ")" << std::endl;
-        client.moveByVelocityZAsync(speed, 0, z, duration, driveTrain, yaw_mode);
+        client.moveByVelocityZAsync(speed, 0, z, duration, driveTrain, yaw_mode, vehicle_name);
         std::this_thread::sleep_for(std::chrono::duration<double>(duration));
         std::cout << "moveByVelocityZ(0, " << speed << "," << z << "," << duration << ")" << std::endl;
-        client.moveByVelocityZAsync(0, speed, z, duration, driveTrain, yaw_mode);
+        client.moveByVelocityZAsync(0, speed, z, duration, driveTrain, yaw_mode, vehicle_name);
         std::this_thread::sleep_for(std::chrono::duration<double>(duration));
         std::cout << "moveByVelocityZ(" << -speed << ", 0, " << z << "," << duration << ")" << std::endl;
-        client.moveByVelocityZAsync(-speed, 0, z, duration, driveTrain, yaw_mode);
+        client.moveByVelocityZAsync(-speed, 0, z, duration, driveTrain, yaw_mode, vehicle_name);
         std::this_thread::sleep_for(std::chrono::duration<double>(duration));
         std::cout << "moveByVelocityZ(0, " << -speed << "," << z << "," << duration << ")" << std::endl;
-        client.moveByVelocityZAsync(0, -speed, z, duration, driveTrain, yaw_mode);
+        client.moveByVelocityZAsync(0, -speed, z, duration, driveTrain, yaw_mode, vehicle_name);
         std::this_thread::sleep_for(std::chrono::duration<double>(duration));
 
-        client.hoverAsync()->waitOnLastTask();
+        client.hoverAsync(vehicle_name)->waitOnLastTask();
 
         std::cout << "Press Enter to land" << std::endl; std::cin.get();
-        client.landAsync()->waitOnLastTask();
+        client.landAsync(60.0F, vehicle_name)->waitOnLastTask();
 
         std::cout << "Press Enter to disarm" << std::endl; std::cin.get();
-        client.armDisarm(false);
+        client.armDisarm(false, vehicle_name);
 
     }
     catch (rpc::rpc_error&  e) {
