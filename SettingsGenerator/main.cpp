b9a1891fd2d4a100afd73dac115a60d29a781e70
racsoraul <racsoraul@gmail.com>
2019-01-25 18:08:08 -0600

    Improved drones formation by specifying lines of drones.

--- SettingsGenerator/main.cpp
+++ SettingsGenerator/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "common/common_utils/FileSystem.hpp"
 #include "common/common_utils/json.hpp"
 
@@ -16,36 +17,61 @@ int main()
     {"SimMode", "Multirotor"},
     {"Vehicles", nullptr}
   };
+
+  string config_root = FileSystem::combine(getenv("SIMBOTIC_ROOT"), "Config");
+  string settings_root = FileSystem::combine(config_root, "AirSim");
+  ofstream settings_file(FileSystem::combine(settings_root, "settings.json"));
+
+  string input;
+  int matrix_size = 5;
+  int drones;
+
+  cout << "How many drones: ";
+  getline(cin, input);
+
+  while (!tryParse(input, drones))
+  {
+    cout << "Error! Must be a number: ";
+    getline(cin, input);
+  }
+
+  cout << "How many drones per lines (Default 5): ";
+  getline(cin, input);
+
+  if (!input.empty()) {
+    while (!tryParse(input, matrix_size))
+    {
+      cout << "Error! Must be a number: ";
+      getline(cin, input);
+    }
+  }
+
+  const int lines = ceil(
+    static_cast<double>(drones)/static_cast<double>(matrix_size)
+  );
+  
+  unsigned short int drone_index = 0;
+  for(unsigned short int i = 0; i < lines; i++)
+  {
+    for(unsigned short int j = 0; j < matrix_size; j++)
+    {
+      settings["Vehicles"]["Drone" + to_string(i) + to_string(j)] = {
+        {"VehicleType", "SimpleFlight"},
+        {"X", j * 4},
+        {"Y", i * -4},
+        {"Z", -2},
+      };
+
+      if (drone_index == drones -1) {
+        break;
+      }
+      ++drone_index;
+    }
+  }
   
   try
   {
-    string config_root = FileSystem::combine(getenv("SIMBOTIC_ROOT"), "Config");
-    string settings_root = FileSystem::combine(config_root, "AirSim");
-    ofstream settings_file(FileSystem::combine(settings_root, "settings.json"));
-
-    cout << "How many drones: ";
-    string input;
-    int drones;
-    getline(cin, input);
-
-    while (!tryParse(input, drones))
-    {
-        cout << "Error! Must be a number: ";
-        getline(cin, input);
-    }
-
-    for(unsigned short int i = 0; i < drones; i++)
-    {
-      settings["Vehicles"]["Drone" + to_string(i)] = {
-        {"VehicleType", "SimpleFlight"},
-        {"X", i * 4},
-        {"Y", 0},
-        {"Z", -2},
-      };
-    }
-
     settings_file << settings.dump(1);
-    
     cout << "settings.json successfully created!" << endl;
   }
   catch(const std::exception& e)
